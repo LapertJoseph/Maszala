c0b92a62bacd05c1201ab21f12bd56e8ed1c2127
LapertJoseph <lapertjoseph@gmail.com>
2023-01-05 22:20:12 +0100

    indent files

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,28 +1,25 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 export default function InputForm(props) {
-
-
-
   return (
     <Box
       component="form"
       sx={{
-        '& > :not(style)': { m: 1 },
+        "& > :not(style)": { m: 1 },
       }}
       noValidate
       autoComplete="off"
     >
-      <TextField 
-      fullWidth={props.fullWidth} 
-      onChange={props.onChange} 
-      label={props.label} 
-      variant={props.variant} 
-      type={props.type} 
-      color={props.color}
-      autoComplete={props.autoComplete}
+      <TextField
+        fullWidth={props.fullWidth}
+        onChange={props.onChange}
+        label={props.label}
+        variant={props.variant}
+        type={props.type}
+        color={props.color}
+        autoComplete={props.autoComplete}
       />
     </Box>
   );
