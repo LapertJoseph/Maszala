398e2c6778e06da3c961fd47119e493205f13efb
LapertJoseph <lapertjoseph@gmail.com>
2023-03-13 00:37:34 +0100

    add useEffec + useNavigate to route the user in Home

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,12 +1,15 @@
 import "../Register/style.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FieldText from "../Ui/TextField";
 import BasicButtons from "../Ui/Button";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [passwordHash, setPassword] = useState("");
+  const [user, setUser] = useState();
 
   const handleChangeEmail = (e) => {
     setEmail(e.target.value);
@@ -14,15 +17,21 @@ const Login = () => {
   const handleChangePassword = (e) => {
     setPassword(e.target.value);
   };
-  const handleSubmit = async(e) => {
-    e.preventDefault();
+  const handleSubmit = async() => {
     try {
       const result = await axios.post('http://localhost:8000/user/auth', {email, passwordHash});
-      console.log(result);
+      console.log(result.data);
+      setUser(result.data)
     } catch (error) {
       console.log(error.message);
     }
   };
+
+  useEffect(() => {
+    if(user) {
+      navigate('/home')
+    }
+  })
 
   return (
     <div className="container-page">
